ea8a647878f6da33ca7851cbcc2af9f4bd31f2f2
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-16 00:54:46 +0800

    Tried to exclude item IO from the top of TutorialStorageBlock.

--- src/main/java/com/lapisliozuli/warex/entities/blockentities/TutorialStorageBlockEntity.java
+++ src/main/java/com/lapisliozuli/warex/entities/blockentities/TutorialStorageBlockEntity.java
@@ -5,12 +5,13 @@ import com.lapisliozuli.warex.WareX;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.collection.DefaultedList;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 
-public class TutorialStorageBlockEntity extends BlockEntity implements ImplementedInventory {
+public class TutorialStorageBlockEntity extends BlockEntity implements ImplementedInventory, SidedInventory {
 
     private final DefaultedList<ItemStack> items = DefaultedList.ofSize(2, ItemStack.EMPTY);
 
@@ -43,5 +44,27 @@ public class TutorialStorageBlockEntity extends BlockEntity implements Implement
     public void fromTag(BlockState state, CompoundTag tag) {
         super.fromTag(state, tag);
         number = tag.getInt("number");
+    }
+
+    @Override
+    public int[] getAvailableSlots(Direction side) {
+        // Just return an array of all slots
+        int[] result = new int[getItems().size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = i;
+        }
+
+        return result;
+    }
+
+    // Cannot insert from the upper side of the block
+    @Override
+    public boolean canInsert(int slot, ItemStack stack, Direction direction) {
+        return direction != Direction.UP;
+    }
+
+    @Override
+    public boolean canExtract(int slot, ItemStack stack, Direction direction) {
+        return true;
     }
 }
