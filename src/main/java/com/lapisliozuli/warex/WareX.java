c4a2c31de0c72293bef3b6f1f01678587765c14b
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-16 11:35:34 +0800

    Added a lot of classes to make the TupperwareBlock into a Dispenser-style inventory.

--- src/main/java/com/lapisliozuli/warex/WareX.java
+++ src/main/java/com/lapisliozuli/warex/WareX.java
@@ -1,14 +1,19 @@
 package com.lapisliozuli.warex;
 
+import com.lapisliozuli.warex.blocks.TupperwareBlock;
 import com.lapisliozuli.warex.blocks.TutorialStorageBlock;
+import com.lapisliozuli.warex.entities.blockentities.TupperwareBlockEntity;
 import com.lapisliozuli.warex.entities.blockentities.TutorialStorageBlockEntity;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.*;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -22,6 +27,25 @@ public class WareX implements ModInitializer {
 	public static final TutorialStorageBlock TUTORIAL_STORAGE = new TutorialStorageBlock(FabricBlockSettings.of(Material.STONE).strength(4.0f));
 	public static BlockEntityType<TutorialStorageBlockEntity> TUTORIAL_STORAGE_ENTITY;
 
+	public static Block TUPPERWARE_BLOCK;
+	public static BlockItem TUPPERWARE_BLOCK_ITEM;
+	public static BlockEntityType<TupperwareBlockEntity> TUPPERWARE_BLOCK_ENTITY;
+	public static final Identifier TUPPERWARE = new Identifier(MOD_ID, "tupperware_block");
+	public static ScreenHandlerType<TupperwareScreenHandler> TUPPERWARE_SCREEN_HANDLER;
+
+
+	static {
+		TUPPERWARE_BLOCK = Registry.register(Registry.BLOCK, TUPPERWARE, new TupperwareBlock(FabricBlockSettings.copyOf(Blocks.CHEST)));
+		TUPPERWARE_BLOCK_ITEM = Registry.register(Registry.ITEM, TUPPERWARE, new BlockItem(TUPPERWARE_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
+
+		//The parameter of build at the very end is always null, do not worry about it
+		TUPPERWARE_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, TUPPERWARE, BlockEntityType.Builder.create(TupperwareBlockEntity::new, TUPPERWARE_BLOCK).build(null));
+
+		//We use registerSimple here because our Entity is not an ExtendedScreenHandlerFactory
+		//but a NamedScreenHandlerFactory.
+		//In a later Tutorial you will see what ExtendedScreenHandlerFactory can do!
+		TUPPERWARE_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(TUPPERWARE, TupperwareScreenHandler::new);
+	}
 
 	@Override
 	public void onInitialize() {
--- src/main/java/com/lapisliozuli/warex/WareXClient.java
+++ src/main/java/com/lapisliozuli/warex/WareXClient.java
@@ -3,10 +3,12 @@ package com.lapisliozuli.warex;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 
 @Environment(EnvType.CLIENT)
 public class WareXClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ScreenRegistry.register(WareX.TUPPERWARE_SCREEN_HANDLER, TupperwareScreen::new);
     }
 }
